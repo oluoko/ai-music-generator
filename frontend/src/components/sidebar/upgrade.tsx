ebbe51fabb204eba58c342672fd9d9e325fb60bc
oluoko <brianotieno586@gmail.com>
2025-08-27 12:48:05 +0300

    Ai Music Generator: Updated upgrade button

--- frontend/src/components/sidebar/upgrade.tsx
+++ frontend/src/components/sidebar/upgrade.tsx
@@ -2,15 +2,12 @@
 
 import { Button } from "@/components/ui/button";
 import { authClient } from "@/lib/auth-client";
+import { env } from "@/env";
 
 export default function Upgrade() {
   const upgrade = async () => {
     await authClient.checkout({
-      products: [
-        "c37323eb-9d27-4851-aefc-801936feec3e",
-        "c6d7906e-95eb-44f1-a3bd-3eaa4df381d7",
-        "101652b9-9daa-4288-ad2d-76f23125adc5",
-      ],
+      products: [env.LARGE_PACK_ID, env.MEDIUM_PACK_ID, env.SMALL_PACK_ID],
     });
   };
   return (
